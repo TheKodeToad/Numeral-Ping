26e2bce43e1bc167b14d15a2212443553d9c71ed
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2021-12-23 15:05:18 +0000

    Add font fix packs

--- src/main/java/me/mcblueparrot/numeralping/NumeralPingMod.java
+++ src/main/java/me/mcblueparrot/numeralping/NumeralPingMod.java
@@ -6,18 +6,32 @@ import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
+import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
 public class NumeralPingMod implements ClientModInitializer {
 
+	public static final String MOD_ID = "numeralping";
 	public static NumeralPingMod INSTANCE;
+
 	private NumeralConfig config;
 
 	@Override
 	public void onInitializeClient() {
 		INSTANCE = this;
+
 		AutoConfig.register(NumeralConfig.class, GsonConfigSerializer::new);
 		config = AutoConfig.getConfigHolder(NumeralConfig.class).getConfig();
+
+		ModContainer container = FabricLoader.getInstance().getModContainer(MOD_ID).get();
+		ResourceManagerHelper.registerBuiltinResourcePack(new Identifier(MOD_ID, "font_fix"), container,
+				ResourcePackActivationType.NORMAL);
+		ResourceManagerHelper.registerBuiltinResourcePack(new Identifier(MOD_ID, "font_fix_high_res"), container,
+				ResourcePackActivationType.NORMAL);
 	}
 
 	public NumeralConfig getConfig() {
