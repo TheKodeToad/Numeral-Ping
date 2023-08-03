d10968b63d18fd70d40202f8d2600c5316cba309
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-08-03 17:59:29 +0200

    more @ModifyArgs usage

--- src/main/java/io/toadlabs/numeralping/mixin/ServerEntryMixin.java
+++ src/main/java/io/toadlabs/numeralping/mixin/ServerEntryMixin.java
@@ -3,9 +3,7 @@ package io.toadlabs.numeralping.mixin;
 import io.toadlabs.numeralping.config.NumeralConfig;
 import io.toadlabs.numeralping.util.Utils;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
 import net.minecraft.client.network.ServerInfo;
 import net.minecraft.text.Text;
@@ -16,7 +14,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 import java.util.List;
 
@@ -25,27 +25,22 @@ import java.util.List;
 @Mixin(value = MultiplayerServerListWidget.ServerEntry.class, priority = 0)
 public class ServerEntryMixin {
 
-	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawText(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;IIIZ)I", ordinal = 0))
-	public int shiftText(DrawContext instance, TextRenderer renderer, Text text, int x, int y, int color, boolean shadow) {
+	@ModifyArgs(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawText(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;IIIZ)I", ordinal = 0))
+	public void shiftText(Args args) {
 		NumeralConfig config = NumeralConfig.instance();
 
 		if (config.serverList) {
-			x += 10 - renderer.getWidth(getPingText(config, server.ping));
+			args.set(2, ((int) args.get(2)) + 10 - client.textRenderer.getWidth(getPingText(config, server.ping)));
 		}
-
-		return instance.drawText(renderer, text, x, y, color, false);
 	}
 
 	// hide the tooltip if it's redundant
-	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/multiplayer/MultiplayerScreen;setMultiplayerScreenTooltip(Ljava/util/List;)V"), require = 0)
-	public void setTooltip(MultiplayerScreen screen, List<Text> tooltip) {
-		if (tooltip != null && tooltip.size() == 1 && NumeralConfig.instance().serverList
-				&& tooltip.get(0).getContent() instanceof TranslatableTextContent translatable
-				&& translatable.getKey().equals("multiplayer.status.ping")) {
-			tooltip = null;
+	@ModifyArgs(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/multiplayer/MultiplayerScreen;setMultiplayerScreenTooltip(Ljava/util/List;)V"), require = 0)
+	public void setTooltip(Args args) {
+		@SuppressWarnings("unchecked") final var tooltip = (List<Text>) args.get(0);
+		if (tooltip != null && tooltip.size() == 1 && NumeralConfig.instance().serverList && tooltip.get(0).getContent() instanceof TranslatableTextContent translatable && translatable.getKey().equals("multiplayer.status.ping")) {
+			args.set(0, null);
 		}
-
-		screen.setMultiplayerScreenTooltip(tooltip);
 	}
 
 	@Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIFFIIII)V", ordinal = 0))
