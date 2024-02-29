5a8fb4e10cc9707f1809b18993bbacfc395f1064
AriDev <75741608+ArikSquad@users.noreply.github.com>
2024-02-29 22:20:15 +0200

    Simplify method

--- src/main/java/io/toadlabs/numeralping/util/Utils.java
+++ src/main/java/io/toadlabs/numeralping/util/Utils.java
@@ -7,37 +7,26 @@ import io.toadlabs.numeralping.config.NumeralConfig;
 public final class Utils {
 
 	public static int getPingColour(int latency) {
-		int level;
-
+		Color colour;
 		NumeralConfig config = NumeralConfig.instance();
 
 		if (latency == -2) {
-			level = latency;
+			colour = config.defaultPingColour;
 		} else if (latency < 0) {
-			level = 5;
+			colour = config.levelFivePingColour;
 		} else if (latency < config.defaultPingThreshold) {
-			level = 0;
+			colour = config.defaultPingColour;
 		} else if (latency < config.levelOnePingThreshold) {
-			level = 1;
+			colour = config.levelOnePingColour;
 		} else if (latency < config.levelTwoPingThreshold) {
-			level = 2;
+			colour = config.levelTwoPingColour;
 		} else if (latency < config.levelThreePingThreshold) {
-			level = 3;
+			colour = config.levelThreePingColour;
 		} else {
-			level = 4;
+			colour = config.levelFourPingColour;
 		}
-
-		// What is this crazy syntax?
-		// Thanks IntelliJ
-		Color colour = switch (level) {
-			case 1 -> config.levelOnePingColour;
-			case 2 -> config.levelTwoPingColour;
-			case 3 -> config.levelThreePingColour;
-			case 4 -> config.levelFourPingColour;
-			case 5 -> config.levelFivePingColour;
-			default -> config.defaultPingColour;
-		};
 
 		return colour.getRGB();
 	}
+
 }
