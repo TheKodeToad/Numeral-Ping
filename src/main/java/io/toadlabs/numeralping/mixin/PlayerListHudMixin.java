1963ad5a6d71d1fbd0daace21a3b229f0fc23d73
JustAlittleWolf <jakobi5000@gmail.com>
2025-07-02 10:13:58 +0200

    fix: ping width extends into names or scoreboard
    fixes #4

--- src/main/java/io/toadlabs/numeralping/mixin/PlayerListHudMixin.java
+++ src/main/java/io/toadlabs/numeralping/mixin/PlayerListHudMixin.java
@@ -1,5 +1,8 @@
 package io.toadlabs.numeralping.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,8 +14,26 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.PlayerListHud;
 import net.minecraft.client.network.PlayerListEntry;
 
+import java.util.List;
+
 @Mixin(PlayerListHud.class)
 public class PlayerListHudMixin {
+
+	@WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Ljava/lang/Math;min(II)I", ordinal = 0))
+	private int adjustTablistEntryWidth(int a, int b, Operation<Integer> original, @Local(ordinal = 0) List<PlayerListEntry> playerListEntries) {
+		NumeralConfig config = NumeralConfig.instance();
+		int vanillaPingWidth = 9;
+		int maxPingWidth = vanillaPingWidth;
+
+		for (PlayerListEntry playerListEntry : playerListEntries) {
+			String pingString = Integer.toString(playerListEntry.getLatency());
+			pingString = config.shiftPing(pingString);
+
+			maxPingWidth = Math.max(maxPingWidth, client.textRenderer.getWidth(pingString));
+		}
+
+		return a + maxPingWidth - vanillaPingWidth;
+	}
 
 	@Inject(method = "renderLatencyIcon", at = @At("HEAD"), cancellable = true)
 	public void renderDetailedLatency(DrawContext context, int width, int x, int y, PlayerListEntry entry,
